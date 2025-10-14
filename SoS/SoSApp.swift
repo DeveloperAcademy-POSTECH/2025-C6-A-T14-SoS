b07028735b20955a5ee025a2fb9dae370eac11cc
Hong Seokho <hongseokhodev@gmail.com>
2025-10-14 18:20:55 +0900

    Initial Commit

---
+++ SoS/SoSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoSApp.swift
+//  SoS
+//
+//  Created by oliver on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SoSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ExpirationDateScannerView()
+        }
+    }
+}
